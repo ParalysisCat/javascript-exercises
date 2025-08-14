d157b5d44c857373985e03b8c3cb3dca514d89c9
ParalysisCat <theonlyirfanismail5151@gmail.com>
2025-08-14 12:34:07 +0800

    13 and 14

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (stringpal) {
+let reduced = stringpal.toLowerCase().replace(/[^a-zA-Z0-9]/g, '').split('')
+let redpal = []
+
+//console.log(reduced)
+for (let i = reduced.length -1  ; i >= 0 ; i--) {
+    redpal.push(reduced[i]);
+    console.log(i + " " + reduced[i]);
+    
+    console.log(redpal)
+
+
+}
+console.log("REDPAL " + redpal)
+console.log("REDUCED " + reduced)
+if (reduced.join('') == redpal.join('')) return true
+else {return false}
 
 };
 
+console.log(palindromes("racecar!"))
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(integer) {
+if (integer < 0) { return "OOPS"} 
+let final = []
+for (let i = 0; i <= integer ; i++){
+    if ( i == 0 ) { final.push(i)
+        console.log(i)
+        console.log(final)
 
+    }
+    else if (i == 1) {final.push(i) 
+        console.log(i)
+        console.log(final)}
+    else {
+        final.push(final[i-1] + final[i-2])
+        console.log(final)
+    }
+    
+}
+return final[integer]
 };
+console.log(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
