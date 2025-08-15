e92442e02e213e2e344efff58d3a0dcb0ae9a665
ParalysisCat <theonlyirfanismail5151@gmail.com>
2025-08-15 15:59:16 +0800

    Finished everything

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(tbooks) {
 
+    let ntitles = []
+
+    for (i = 0; i < tbooks.length ; i++ ) {
+        ntitles.push(tbooks[i].title)
+        console.log(ntitles)
+    }
+    return ntitles
 };
 
+console.log(getTheTitles([{title: 'Book',author: 'Name'},{title: 'Book2',author: 'Name2'}]))
 // Do not edit below this line
 module.exports = getTheTitles;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
+    let agecal = 0
+    let max = 0
+    let slot = 0
+    for (let i = 0; i < people.length; i++ ){ 
+        if (people[i].hasOwnProperty('yearOfDeath')){
+            agecal = people[i].yearOfDeath - people[i].yearOfBirth
+            if (agecal >= max) { 
+                max = agecal
+                slot = i
+            }
+        }
+        else {
+            agecal = new Date().getFullYear() - people[i].yearOfBirth
+            if (agecal >= max) { 
+                max = agecal
+                slot = i
+            }
+        }
+        
+    }
+    return people[slot]
 };
 
 // Do not edit below this line
